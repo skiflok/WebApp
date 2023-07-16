0b4cfb59faf30a5e5c624814be7642e835fccbc9
violator <violator@student.21-school.ru>
2023-07-16 17:25:30 +0300

    fix

--- src/main/java/org/example/repositories/MessageRepository.java
+++ src/main/java/org/example/repositories/MessageRepository.java
@@ -2,7 +2,6 @@ package org.example.repositories;
 
 import org.example.model.Message;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 public interface MessageRepository extends CrudRepository<Message, Long> {
 
