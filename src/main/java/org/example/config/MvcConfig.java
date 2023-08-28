8ecb130168ae19965cb89b9ac0ec8b63a94e0e95
violator <violator@student.21-school.ru>
2023-08-28 12:33:25 +0300

    change url to images

--- src/main/java/org/example/config/MvcConfig.java
+++ src/main/java/org/example/config/MvcConfig.java
@@ -22,9 +22,9 @@ public class MvcConfig implements WebMvcConfigurer {
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry.addResourceHandler("/images/**")
-        .addResourceLocations("file:" + uploadPath + "/");
-    registry.addResourceHandler("/uploads/**")
-        .addResourceLocations("file:" + uploadPath + "/");
+        .addResourceLocations("file://" + uploadPath + "/");
+//    registry.addResourceHandler("/uploads/**")
+//        .addResourceLocations("file:" + uploadPath + "/");
   }
 
 
